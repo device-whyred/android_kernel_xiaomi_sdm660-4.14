1e520a398d83acb527214c39c10d77d41bc9752e
mohammad iqbal <mi460334@gmail.com>
2020-12-01 15:14:59 +0000

    remove

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 0 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#0 SMP PREEMPT Tue Dec 1 14:08:12 UTC 2020"
-#define LINUX_COMPILE_BY "iqbal"
-#define LINUX_COMPILE_HOST "whyred"
-#define LINUX_COMPILER "NusantaraDevs clang version 11.0.0"
