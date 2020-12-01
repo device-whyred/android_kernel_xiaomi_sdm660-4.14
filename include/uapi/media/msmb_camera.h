b2abaee68e0d85051e7a55327dcb4e950db9341e
mohammad iqbal <mi460334@gmail.com>
2020-12-01 09:23:50 +0000

    Revert "msm: camera: Bring back old subdev ids"
    This reverts commit e1e30adf0817d873d494300edf4a2963ce9da880.

--- include/uapi/media/msmb_camera.h
+++ include/uapi/media/msmb_camera.h
@@ -59,8 +59,6 @@
 #else
 #define MSM_MAX_CAMERA_SENSORS  6
 #endif
-#define MSM_MAX_CAMERA_SENSORS  5
-#endif
 
 /* The below macro is defined to put an upper limit on maximum
  * number of buffer requested per stream. In case of extremely
